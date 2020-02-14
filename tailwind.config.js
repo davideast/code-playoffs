112d34d84aafce4453042eccb3d60601f579e86f
David East <deast@google.com>
2020-02-14 06:12:50 -0700

    .

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'blue-light': '#1EA1F3',
+        'orange-dark': '#E78365',
+        'orange': '#F3A838',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
